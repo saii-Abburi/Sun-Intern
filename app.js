7a9835e42f1172eeec25766f8b23c57a60ccbf44
Saiinaruto <saipraveenabburi7@gmail.com>
2025-05-01 20:48:48 +0530

    added2sections

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector('.slider');
 const navLinks = document.querySelectorAll('.slider-nav a');
 const slides = document.querySelectorAll('.slider img');
-
+const buttons = document.querySelectorAll('.about-tags button');
 let currentIndex = 0;
 
 function updateActiveLink(index) {
@@ -26,7 +26,7 @@ function autoScroll() {
     updateActiveLink(currentIndex);
 }
 
-setInterval(autoScroll, 3000);
+setInterval(autoScroll, 4000);
 
 navLinks.forEach((link, index) => {
     link.addEventListener('click', () => {
@@ -47,5 +47,26 @@ function toggleMenu(){
     }
 }
 
-const links = document.getElementsByClassName('slider-nav')[0].children;
-console.log(links)
+function aboutUs(){
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
+    });
+  });
+}
+aboutUs();
+
+const infos = document.querySelectorAll(".abt-tag-info");
+
+  buttons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      // Toggle active button
+      buttons.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+
+      // Toggle info section
+      infos.forEach(info => info.classList.remove("active"));
+      infos[index].classList.add("active");
+    });
+  });
